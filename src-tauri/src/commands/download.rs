428e9350916e32916370bc3490d389b19157e8ad
NullCode1337 <ishaqahmed.dev@gmail.com>
2025-10-18 00:01:28 +0600

    rust: Only cancel download when gallery-dl error is present

--- src-tauri/src/commands/download.rs
+++ src-tauri/src/commands/download.rs
@@ -100,7 +100,7 @@ async fn gallery_dl(app: tauri::AppHandle, link: &str) -> Result<(), Box<dyn std
     let stderr_handle = tokio::spawn(async move {
         while let Ok(Some(line)) = stderr_reader.next_line().await {
             println!("{:#}", &line);
-            if line.contains("error") {
+            if line.contains("[error") {
                 app_stderr.emit("download-error", &line).unwrap();
             } else {
                 app_stderr.emit("notification", &line).unwrap();
