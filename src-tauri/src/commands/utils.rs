4519d50fe8713486bffb633f7d95a756035fb11d
NullCode1337 <contact.nullcode@proton.me>
2026-05-14 10:42:20 +0600

    rust: Add notification command

--- src-tauri/src/commands/utils.rs
+++ src-tauri/src/commands/utils.rs
@@ -211,3 +211,32 @@ pub async fn get_free_space(app: tauri::AppHandle) -> Result<f64, String> {
         Err("Could not determine disk usage".to_string())
     }
 }
+
+#[tauri::command]
+pub async fn notify(_app: tauri::AppHandle, body: String) -> Result<(), String> {
+    let title = "MediaMagnet";
+
+    #[cfg(target_os = "linux")]
+    {
+        use std::process::Command;
+        Command::new("notify-send")
+            .arg("--app-name=MediaMagnet")
+            .arg(title)
+            .arg(&body)
+            .spawn()
+            .map_err(|e| format!("Notification error: {e}"))?;
+    }
+
+    #[cfg(not(target_os = "linux"))]
+    {
+        use tauri_plugin_notification::NotificationExt;
+        _app.notification()
+            .builder()
+            .title(title)
+            .body(&body)
+            .show()
+            .map_err(|e| format!("Notification error: {e}"))?;
+    }
+
+    Ok(())
+}
