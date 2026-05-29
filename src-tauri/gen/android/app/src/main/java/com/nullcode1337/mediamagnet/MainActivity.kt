c9b2dbb19882991fa6ea9891dde1475f90293c0a
NullCode1337 <contact.nullcode@proton.me>
2026-05-30 00:00:43 +0600

    android: Prevent app from going underneath status bar

--- src-tauri/gen/android/app/src/main/java/com/nullcode1337/mediamagnet/MainActivity.kt
+++ src-tauri/gen/android/app/src/main/java/com/nullcode1337/mediamagnet/MainActivity.kt
@@ -2,10 +2,21 @@ package com.nullcode1337.mediamagnet
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : TauriActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     enableEdgeToEdge()
     super.onCreate(savedInstanceState)
+
+    ViewCompat.setOnApplyWindowInsetsListener(window.decorView) { view, insets ->
+      val bars = insets.getInsets(
+        WindowInsetsCompat.Type.systemBars() or
+        WindowInsetsCompat.Type.displayCutout()
+      )
+      view.setPadding(bars.left, bars.top, bars.right, bars.bottom)
+      insets
+    }
   }
 }
